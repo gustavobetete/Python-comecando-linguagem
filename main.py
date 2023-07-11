0c8a45754cd770885c95964797165e3530f9fa01
gustavobetete <gustavobetete@hotmail.com>
2023-07-11 09:23:56 -0300

    Simplificando codigo do jogo

--- main.py
+++ main.py
@@ -10,13 +10,16 @@ print("Voce digitou ", chute_str)
 
 chute = int(chute_str)
 
-if chute > numero_secreto:
-    print("Voce errou! O seu chute foi MAIOR que o numero secreto")
+acertou = chute == numero_secreto
+maior = chute > numero_secreto
+menor = chute < numero_secreto
 
-if chute < numero_secreto:
-    print("Voce errou! O seu chute foi MENOR que o numero secreto")
-
-if chute == numero_secreto:
+if acertou:
     print("Voce acertou!!!")
+else:
+    if maior:
+        print("Voce errou! O seu chute foi MAIOR que o numero secreto")
+    elif menor:
+        print("Voce errou! O seu chute foi MENOR que o numero secreto")
 
 print("Fim do jogo")
